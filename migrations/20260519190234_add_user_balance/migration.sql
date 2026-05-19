9f72f6884108385d35838776b6b32a3215d1b072
Mykhailo <smykmsha@gmail.com>
2026-05-19 22:23:34 +0300

    Delete migrations directory

--- migrations/20260519190234_add_user_balance/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "balance" DOUBLE PRECISION NOT NULL DEFAULT 0.0;
--- migrations/20260519190534_drop_user_phone/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `user_phone` on the `users` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "users" DROP COLUMN "user_phone";
