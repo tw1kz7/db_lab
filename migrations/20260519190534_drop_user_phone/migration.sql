869e56d9e81d0abcedc9e35cf9372f65400337ea
Mykhailo <smykmsha@gmail.com>
2026-05-19 22:21:43 +0300

    Add files via upload

---
+++ migrations/20260519190534_drop_user_phone/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `user_phone` on the `users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "user_phone";
